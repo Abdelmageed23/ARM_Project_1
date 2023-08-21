5224a96e9324b547ad8e092e9bdb44948be9bbe1
HaninAnwar <HaninAnwar555@gmail.com>
2023-08-22 00:29:42 +0300

    Add GPIO f103 registers

--- F103/D_LIB/stm32f103xx.h
+++ F103/D_LIB/stm32f103xx.h
@@ -39,5 +39,37 @@ typedef struct
 
 #define NVIC                                  ((NVIC_REG*)NVIC_BASE_ADDRESS)
 
+/************************************************************************************************/
+/******************************** GPIO Registers ************************************************/
+/************************************************************************************************/
+#define GPIOA_BASE_ADDRESS       	0x40010800U
+#define GPIOB_BASE_ADDRESS       	0x40010C00U
+#define GPIOC_BASE_ADDRESS       	0x40011000U
+#define GPIOD_BASE_ADDRESS       	0x40011400U
+#define GPIOE_BASE_ADDRESS       	0x40011800U
+#define GPIOF_BASE_ADDRESS       	0x40011C00U
+#define GPIOG_BASE_ADDRESS       	0x40012000U
+
+
+typedef struct
+{
+    volatile    uint32_t  CRL ;
+    volatile    uint32_t  CRH ;
+    volatile    uint32_t  IDR  ;
+    volatile    uint32_t  ODR  ;
+    volatile    uint32_t  BSRR  ;
+    volatile    uint32_t  BRR  ;
+    volatile    uint32_t  LCKR  ;
+
+}GPIO_RegDef_T;
+
+
+#define GPIOA            ((GPIO_RegDef_T  *)GPIOA_BASE_ADDRESS)
+#define GPIOB            ((GPIO_RegDef_T  *)GPIOB_BASE_ADDRESS)
+#define GPIOC            ((GPIO_RegDef_T  *)GPIOC_BASE_ADDRESS)
+#define GPIOD            ((GPIO_RegDef_T  *)GPIOD_BASE_ADDRESS)
+#define GPIOE            ((GPIO_RegDef_T  *)GPIOE_BASE_ADDRESS)
+#define GPIOF            ((GPIO_RegDef_T  *)GPIOF_BASE_ADDRESS)
+#define GPIOG            ((GPIO_RegDef_T  *)GPIOG_BASE_ADDRESS)
 
 #endif
