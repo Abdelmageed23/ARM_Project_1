0dbc79651e1518f8feee33339f76953a12b9dc1e
Diea Abdeltwab <da1703@fayoum.edu.eg>
2023-08-21 12:41:46 -0700

    add -->BIT_MATH and ERROR_STATE

---
+++ F446/D_LIB/ERROR_STATE.h
@@ -0,0 +1,17 @@
+
+#ifndef  ERROR_STATE_H
+#define  ERROR_STATE_H
+
+
+#define NULL ((void*)0)
+
+
+
+
+#define ERROR_STATE_NOK         0
+#define ERROR_STATE_OK          1
+#define ERROR_STATE_NULL_PTR    2
+
+
+
+#endif
