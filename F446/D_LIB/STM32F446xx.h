a80c161a23bf3cde076a82498b013e782686b788
Diea Abdeltwab <da1703@fayoum.edu.eg>
2023-08-21 12:50:21 -0700

    libraries for STM32F446xx

--- F446/D_LIB/STM32F446xx.h
+++ F446/D_LIB/STM32F446xx.h
@@ -27,5 +27,45 @@ typedef struct
 /*********************  DMA Register Definition    *********************/
 #define DMA1					((DMA_RegDef_T *)DMA1_BASE_ADRESS)
 #define DMA2					((DMA_RegDef_T *)DMA2_BASE_ADRESS)
+/************************************************************************************************/
+/********************************* SYSTICK Registers ********************************************/
+/************************************************************************************************/
+#define STK_BASE_ADDRESS                                  0xE000E010UL
+
+
+typedef struct
+{
+	volatile uint32_t CTRL;
+	volatile uint32_t LOAD;
+	volatile uint32_t VAL ;
+	volatile uint32_t CALIB;
+}STK_REG;
+
+#define STK                                  ((STK_REG*)STK_BASE_ADDRESS)
+
+/************************************************************************************************/
+/******************************** NVIC Registers ************************************************/
+/************************************************************************************************/
+#define NVIC_u32_BASE_ADDRESS                               0xE000E100UL
+
+typedef struct
+{
+	volatile uint32_t ISER[8];		/*Interrupt set-enable register*/
+	uint32_t Reserved1[24];
+	volatile uint32_t ICER[8];		/*Interrupt clear-enable register*/
+	uint32_t Reserved[24];
+	volatile uint32_t ISPR[8];		/*Interrupt set-pending register*/
+	uint32_t Reserved2[24];
+	volatile uint32_t ICPR[8];		/*Interrupt clear-pending register*/
+	uint32_t Reserved4[24];
+	volatile uint32_t IABR[8];		/*Interrupt active bit register*/
+	uint32_t Reserved3[32];
+	volatile uint8_t IPR[240];		/*Interrupt priority register*/
+}NVIC_REG;
+
+#define NVIC                                  ((NVIC_REG*)NVIC_u32_BASE_ADDRESS)
+
+
+
 
 #endif
