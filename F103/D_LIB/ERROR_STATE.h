2710e1b06154f711fe1919fb44c7a90d403dc7b2
Diea Abdeltwab <da1703@fayoum.edu.eg>
2023-08-21 12:53:34 -0700

    libraries for STM32F103C8T6

---
+++ F103/D_LIB/ERROR_STATE.h
@@ -0,0 +1,29 @@
+/**
+ ******************************************************************************
+ * @file           : ERROR_STATE.h
+ * @author         : Diea Abdeltwab
+ * @layer          : Library
+ * @brief          : Version 2 For STM32F103C8T6 Drivers
+ * @date           : 25/7/2023
+ ******************************************************************************
+ * @attention
+ *
+ *
+ ******************************************************************************
+ */
+#ifndef  ERROR_STATE_H
+#define  ERROR_STATE_H
+
+
+#define NULL ((void*)0)
+
+
+
+
+#define ERROR_STATE_NOK         0
+#define ERROR_STATE_OK          1
+#define ERROR_STATE_NULL_PTR    2
+
+
+
+#endif
